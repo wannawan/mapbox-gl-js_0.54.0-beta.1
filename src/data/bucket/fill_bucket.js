72ec301e77bc338c57c8f84b82fe2f2f985221ad
Na Wang <lunawang889@gmail.com>
2020-09-17 21:02:09 -0400

    fix bug of small self-intersection

--- src/data/bucket/fill_bucket.js
+++ src/data/bucket/fill_bucket.js
@@ -1,17 +1,18 @@
 // @flow
 
-import { FillLayoutArray } from '../array_types';
+import {FillLayoutArray} from '../array_types';
 import libtess from 'libtess/libtess.debug.js'
 
-import { members as layoutAttributes } from './fill_attributes';
+import {members as layoutAttributes} from './fill_attributes';
 import SegmentVector from '../segment';
-import { ProgramConfigurationSet } from '../program_configuration';
-import { LineIndexArray, TriangleIndexArray } from '../index_array_type';
+import {ProgramConfigurationSet} from '../program_configuration';
+import {LineIndexArray, TriangleIndexArray} from '../index_array_type';
 import earcut from 'earcut';
 import classifyRings from '../../util/classify_rings';
 import assert from 'assert';
+
 const EARCUT_MAX_RINGS = 500;
-import { register } from '../../util/web_worker_transfer';
+import {register} from '../../util/web_worker_transfer';
 import {hasPattern, addPatternDependencies} from './pattern_bucket_features';
 import loadGeometry from '../load_geometry';
 import EvaluationParameters from '../../style/evaluation_parameters';
@@ -106,12 +107,12 @@ class FillBucket implements Bucket {
         }
     }
 
-    update(states: FeatureStates, vtLayer: VectorTileLayer, imagePositions: {[string]: ImagePosition}) {
+    update(states: FeatureStates, vtLayer: VectorTileLayer, imagePositions: { [string]: ImagePosition }) {
         if (!this.stateDependentLayers.length) return;
         this.programConfigurations.updatePaintArrays(states, vtLayer, this.stateDependentLayers, imagePositions);
     }
 
-    addFeatures(options: PopulateParameters, imagePositions: {[string]: ImagePosition}) {
+    addFeatures(options: PopulateParameters, imagePositions: { [string]: ImagePosition }) {
         for (const feature of this.features) {
             const {geometry} = feature;
             this.addFeature(feature, geometry, feature.index, imagePositions);
@@ -125,6 +126,7 @@ class FillBucket implements Bucket {
     uploadPending(): boolean {
         return !this.uploaded || this.programConfigurations.needsUpload;
     }
+
     upload(context: Context) {
         if (!this.uploaded) {
             this.layoutVertexBuffer = context.createVertexBuffer(this.layoutVertexArray, layoutAttributes);
@@ -145,7 +147,12 @@ class FillBucket implements Bucket {
         this.segments2.destroy();
     }
 
-    addFeature(feature: BucketFeature, geometry: Array<Array<Point>>, index: number, imagePositions: {[string]: ImagePosition}) {
+    addFeature(feature: BucketFeature, geometry: Array<Array<Point>>, index: number, imagePositions: { [string]: ImagePosition }) {
+        //var target_area = 49037, is_target = false;
+        // if (feature.properties.area === target_area) {
+        //     is_target = true;
+        // }
+
         for (const polygon of classifyRings(geometry, EARCUT_MAX_RINGS)) {
             let numVertices = 0;
             for (const ring of polygon) {
@@ -186,81 +193,131 @@ class FillBucket implements Bucket {
                 lineSegment.primitiveLength += ring.length;
             }
 
-            //let  indices = earcut(flattened, holeIndices);
 
-            const vertexCallback = (data, polyVertArray)  => {
-                polyVertArray[polyVertArray.length] = data[0];
-                polyVertArray[polyVertArray.length] = data[1];
-            };
+            let indices;
+            if (false) {
+                indices = earcut(flattened, holeIndices);
+            } else {
+                const vertexCallback = (data, polyVertArray) => {
+                    polyVertArray[polyVertArray.length] = data[0];
+                    polyVertArray[polyVertArray.length] = data[1];
+                };
 
-            const begincallback = (type) => {
-                if (type !== libtess.primitiveType.GL_TRIANGLES) {
-                    console.log('expected TRIANGLES but got type: ' + type);
-                }
-            };
+                const begincallback = (type) => {
+                    if (type !== libtess.primitiveType.GL_TRIANGLES) {
+                        console.log('expected TRIANGLES but got type: ' + type);
+                    }
+                };
 
-            const errorcallback = (errno)  => {
-                console.log('error callback');
-                console.log('error number: ' + errno);
-            };
+                const errorcallback = (errno) => {
+                    console.log('error callback');
+                    console.log('error number: ' + errno);
+                };
 
-            const combinecallback = (coords, data, weight) => {
-                //console.log('combine callback');
-                return [coords[0], coords[1], coords[2]];
-            };
+                const combinecallback = (coords, data, weight) => {
+                    return [coords[0], coords[1], coords[2]];
+                };
 
-            const edgeCallback = (flag) =>  {
-                // don't really care about the flag, but need no-strip/no-fan behavior
-                // console.log('edge flag: ' + flag);
-            };
+                const edgeCallback = (flag) => {
+                    // don't really care about the flag, but need no-strip/no-fan behavior
+                    // console.log('edge flag: ' + flag);
+                };
 
-            let contours = [],
-                point_map = {},
-                point_count = 0,
-                tessy = new libtess.GluTesselator(),
-                triangleVerts = [],
-                new_indices = [];
+                let contours = [], point_map = {}, point_count = 0,
+                    tessy = new libtess.GluTesselator(), triangleVerts = [],
+                    new_indices = [],
+                    infinite = 300000, min_x = infinite, max_x = -infinite, min_y = infinite, max_y = -infinite;
 
-            polygon.map((poly) => {
-                let new_poly = [];
-                const poly_len = poly.length;
-                poly.map((point, index) =>{
-                    new_poly.push(point.x);
-                    new_poly.push(point.y);
-                    point_map[point.x*8192 + point.y] = point_count;
-                    point_count +=1;
+                polygon.map((poly) => {
+                    let new_poly = [];
+                    const poly_len = poly.length;
+                    poly.map((point, index) => {
+                        new_poly.push(point.x);
+                        new_poly.push(point.y);
+                        // get point xy range
+                        min_x = Math.min(min_x, point.x);
+                        max_x = Math.max(max_x, point.x);
+                        min_y = Math.min(min_y, point.y);
+                        max_y = Math.max(max_y, point.y);
+
+                        if (index !== poly_len - 1) {
+                            point_map[point.x * 8192 + point.y] = point_count;
+                        }
+                        point_count += 1;
+                    });
+                    contours.push(new_poly);
                 });
-                contours.push(new_poly);
-            });
 
-            tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_VERTEX_DATA, vertexCallback);
-            tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_BEGIN, begincallback);
-            tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_ERROR, errorcallback);
-            tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_COMBINE, combinecallback);
-            tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_EDGE_FLAG, edgeCallback);
+                tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_VERTEX_DATA, vertexCallback);
+                tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_BEGIN, begincallback);
+                tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_ERROR, errorcallback);
+                tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_COMBINE, combinecallback);
+                tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_EDGE_FLAG, edgeCallback);
 
-            tessy.gluTessNormal(0, 0, 1);
+                tessy.gluTessNormal(0, 0, 1);
 
-            tessy.gluTessBeginPolygon(triangleVerts);
+                tessy.gluTessBeginPolygon(triangleVerts);
 
-            for (var i = 0; i < contours.length; i++) {
-                tessy.gluTessBeginContour();
-                var contour = contours[i];
-                for (var j = 0; j < contour.length; j += 2) {
-                    var coords = [contour[j], contour[j + 1], 0];
-                    tessy.gluTessVertex(coords, coords);
+                for (var i = 0; i < contours.length; i++) {
+                    tessy.gluTessBeginContour();
+                    var contour = contours[i];
+                    for (var j = 0; j < contour.length; j += 2) {
+                        var coords = [contour[j], contour[j + 1], 0];
+                        tessy.gluTessVertex(coords, coords);
+                    }
+                    tessy.gluTessEndContour();
                 }
-                tessy.gluTessEndContour();
-            }
-            tessy.gluTessEndPolygon();
+                tessy.gluTessEndPolygon();
 
-            for (let i=0; i<triangleVerts.length; i=i+2) {
-                let map_index = triangleVerts[i]*8192+triangleVerts[i+1];
-                let p_i =point_map[map_index];
-                new_indices.push(p_i);
-            }
+                for (let i = 0; i < triangleVerts.length; i = i + 2) {
+                    let map_index = triangleVerts[i] * 8192 + triangleVerts[i + 1];
+                    let p_i = point_map[map_index];
+                    if (p_i === undefined) {
+                        // find nearest one
+                        let vert_x = Math.floor(triangleVerts[i]), vert_y = Math.floor(triangleVerts[i + 1]),
+                            distance = 1, match_x, match_y, find_match = false,
+                            match_cases = [[-1, -1], [-1, 1], [1, -1], [1, 1]], m, k;
+                        while (true) {
+                            if (vert_x + distance > max_x && vert_x - distance < min_x && vert_y + distance > max_y && vert_y - distance < min_y) {
+                                break;
+                            }
+                            for (m = 0; m <= distance; m++) {
+                                for (k = 0; k <= distance; k++) {
+                                    if (m <= distance - 1 && k <= distance - 1) continue;
+                                    for (let m_case_index in match_cases) {
+                                        let m_case = match_cases[m_case_index];
+                                        match_x = vert_x + m_case[0] * m;
+                                        match_y = vert_y + m_case[1] * k;
+                                        if (point_map[match_x * 8192 + match_y] !== undefined) {
+                                            find_match = true;
+                                            break;
+                                        }
+                                    }
+                                    if (find_match) break;
+                                }
+                                if (find_match) break;
+                            }
 
-             let indices = new_indices;
+                            if (find_match) {
+                                break
+                            } else {
+                                distance += 1;
+                            }
+                        }
+
+                        if (find_match) {
+                            new_indices.push(point_map[match_x * 8192 + match_y]);
+                        } else {
+                            console.error("not find any nearby point");
+                            return
+                        }
+                    } else {
+                        new_indices.push(p_i);
+                    }
+                }
+
+                indices = new_indices;
+            }
 
             assert(indices.length % 3 === 0);
 
@@ -277,43 +334,43 @@ class FillBucket implements Bucket {
         this.programConfigurations.populatePaintArrays(this.layoutVertexArray.length, feature, index, imagePositions);
     }
 
-    xxx () {
-                    // let combined_polygons = [];
-            // for (let p in polygon) {
-            //     combined_polygons = combined_polygons.concat(polygon[p]);
-            // }
-            //
-            // console.log('combined_polygons', combined_polygons)
-            // let polygons = [];
-            // for (let i = 0; i < indices.length; i = i+3) {
-            //     let indice1 = indices[i];
-            //     let indice2 = indices[i + 1];
-            //     let indice3 = indices[i + 2];
-            //
-            //     let poly= [];
-            //     poly.push(combined_polygons[indice1])
-            //     poly.push(combined_polygons[indice2])
-            //     poly.push(combined_polygons[indice3])
-            //     polygons.push(poly)
-            // }
-            // console.log("new polygon", polygons);
-            // let st_polygons = [];
-            // //st_polygons.push('(8192 0, 8192 8192, 0 8192, 0 0, 8192 0)')
-            // for (let i in polygons){
-            //     let poly = polygons[i];
-            //     let st_polygon = [];
-            //     //console.log("polygon", polygon)
-            //     for (let j in poly) {
-            //         let point = poly[j];
-            //        // console.log('point', point)
-            //         st_polygon.push(point.x + ' ' + point.y)
-            //     }
-            //     st_polygon.push(poly[0].x + ' ' + poly[0].y)
-            //
-            //     st_polygons.push('('+st_polygon.join(', ')+')')
-            // }
-            //
-            // console.log(st_polygons.join(', '));
+    xxx() {
+        // let combined_polygons = [];
+        // for (let p in polygon) {
+        //     combined_polygons = combined_polygons.concat(polygon[p]);
+        // }
+        //
+        // console.log('combined_polygons', combined_polygons)
+        // let polygons = [];
+        // for (let i = 0; i < indices.length; i = i+3) {
+        //     let indice1 = indices[i];
+        //     let indice2 = indices[i + 1];
+        //     let indice3 = indices[i + 2];
+        //
+        //     let poly= [];
+        //     poly.push(combined_polygons[indice1])
+        //     poly.push(combined_polygons[indice2])
+        //     poly.push(combined_polygons[indice3])
+        //     polygons.push(poly)
+        // }
+        // console.log("new polygon", polygons);
+        // let st_polygons = [];
+        // //st_polygons.push('(8192 0, 8192 8192, 0 8192, 0 0, 8192 0)')
+        // for (let i in polygons){
+        //     let poly = polygons[i];
+        //     let st_polygon = [];
+        //     //console.log("polygon", polygon)
+        //     for (let j in poly) {
+        //         let point = poly[j];
+        //        // console.log('point', point)
+        //         st_polygon.push(point.x + ' ' + point.y)
+        //     }
+        //     st_polygon.push(poly[0].x + ' ' + poly[0].y)
+        //
+        //     st_polygons.push('('+st_polygon.join(', ')+')')
+        // }
+        //
+        // console.log(st_polygons.join(', '));
 
     }
 }
